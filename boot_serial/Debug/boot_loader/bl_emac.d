bd3f0307b47c5a4cb481e5d1a3278732519aae15
alex1111 <1490434676@qq.com>
2022-07-04 21:26:01 +0800

    first

---
+++ boot_serial/Debug/boot_loader/bl_emac.d
@@ -0,0 +1,96 @@
+# FIXED
+
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/boot_loader/bl_emac.c
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/string.h
+boot_loader/bl_emac.obj: C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/xlocale/_string.h
+boot_loader/bl_emac.obj: C:/Users/alex/Desktop/boot/boot_serial/bl_config.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_emac.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_flash.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_nvic.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_sysctl.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/pin_map.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/emac.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/boot_loader/bl_decrypt.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/boot_loader/bl_flash.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/rom.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/boot_loader/bl_hooks.h
+boot_loader/bl_emac.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/rom_map.h
+
+C:/ti/TivaWare_C_Series-2.2.0.295/boot_loader/bl_emac.c:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/string.h:
+
+C:/ti/ccs1120/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/xlocale/_string.h:
+
+C:/Users/alex/Desktop/boot/boot_serial/bl_config.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_emac.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_flash.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_nvic.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_sysctl.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/pin_map.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/emac.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/boot_loader/bl_decrypt.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/boot_loader/bl_flash.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/rom.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/boot_loader/bl_hooks.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/rom_map.h:
+
